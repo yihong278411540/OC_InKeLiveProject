b54d65e30192c4d01285a414d2d1f6cfe23388ca
yh835269661 <835269661@qq.com>
2018-06-22 11:56:21 +0800

    v1.0.1 add podfile

---
+++ InKeLiveBroadcast/Pods/Target Support Files/Pods-InKeLiveBroadcastTests/Pods-InKeLiveBroadcastTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InKeLiveBroadcastTests : NSObject
+@end
+@implementation PodsDummy_Pods_InKeLiveBroadcastTests
+@end
---
+++ InKeLiveBroadcast/Pods/Target Support Files/Pods-InKeLiveBroadcastUITests/Pods-InKeLiveBroadcastUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InKeLiveBroadcastUITests : NSObject
+@end
+@implementation PodsDummy_Pods_InKeLiveBroadcastUITests
+@end
